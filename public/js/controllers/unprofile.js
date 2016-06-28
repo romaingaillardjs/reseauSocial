d211d49a78dde0d329eb33b19e1cfcad35d4e1c1
romaingaillard <romain.gaillard.ifocop@gmail.com>
2016-06-28 13:35:34 +0200

    update 28/06/2016

--- public/js/controllers/unprofile.js
+++ public/js/controllers/unprofile.js
@@ -1,7 +1,9 @@
 angular.module('MyApp')
 	.controller('ProfilCtrl', function($scope, $rootScope, $location, $window, $auth, $routeParams, Search) {
   		$scope.displayProfil = function () {
-  			return Search.searchById($routeParams.params).then(function (data) {$scope.user = data})
+  			return Search.searchById($routeParams.params).then(function (data) {$scope.user = angular.fromJson(data)
+  				console.log($scope.user)
+  			})
   		};
   		$scope.displayProfil();
 });
