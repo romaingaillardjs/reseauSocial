4b6b3086dc782bb5623e8ec0b221d891d7bf6162
romaingaillard <romain.gaillard.ifocop@gmail.com>
2016-07-01 10:15:28 +0200

    01/07/2016

--- public/js/controllers/listeAmis.js
+++ public/js/controllers/listeAmis.js
@@ -1,5 +1,5 @@
 angular.module('MyApp')
-  .controller('ListeAmisCtrl',['$scope', '$timeout','$log','$q','$rootScope', '$sce','$sanitize','amis' function($scope, $timeout,$log,$q,$rootScope,$sce, $sanitize,amis) {
+  .controller('ListeAmisCtrl',['$scope', '$timeout','$log','$q','$rootScope', '$sce','$sanitize','Amis' function($scope, $timeout,$log,$q,$rootScope,$sce, $sanitize,Amis) {
     $scope.listeAmis = function (data) {
       Amis.listeAmisRequest().success(function (data) {
             $scope.amis = data
