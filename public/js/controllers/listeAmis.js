07d404f387839dc4e9aa1dd5d95f5b21a4de8762
romaingaillard <romain.gaillard.ifocop@gmail.com>
2016-07-01 10:09:04 +0200

    01/07/2016

--- public/js/controllers/listeAmis.js
+++ public/js/controllers/listeAmis.js
@@ -1,27 +1,11 @@
 angular.module('MyApp')
-            
-.controller('ListeAmisCtrl',['$scope', '$timeout','$log','$q','$rootScope', '$sce','$sanitize', function($scope, $timeout,$log,$q,$rootScope,$sce, $sanitize) {
-        $scope.renderHtml = function (htmlCode) {
-            return $sce.trustAsHtml(htmlCode);
-        };
-
-  
-
-
-
-
-  $scope.activeButton = function(arg) {
-    var test = document.getElementById('id'+arg).classList;
-  if ( test[0] == "FAQclose" || test[1] == "FAQclose" )
-  {
-    $scope.locale = test.remove("FAQclose");
-    $scope.locale = test.add("FAQopen");
-  } else {
-    $scope.locale = test.remove("FAQopen");
-    $scope.locale = test.add("FAQclose");
-  }  
-  }  
-
+  .controller('ListeAmisCtrl',['$scope', '$timeout','$log','$q','$rootScope', '$sce','$sanitize','amis' function($scope, $timeout,$log,$q,$rootScope,$sce, $sanitize,amis) {
+    $scope.listeAmis = function (data) {
+      Amis.listeAmisRequest().success(function (data) {
+            $scope.amis = data
+          });    
+          return $scope.amis;
+      }
 }])
 
 
--- public/js/services/amis.js
+++ public/js/services/amis.js
@@ -1,57 +1,14 @@
-(function(){
-  'use strict';
-
 angular.module('MyApp')
-         .service('amisService', function (){
-    
-    var AMIS = [
-    {
-      index:0,
-      titre: 'michel' , 
-      status : 'confirmé',
-      name:'MyName',
-      img:'http://img0.mxstatic.com/wallpapers/44e535006cffbc1b6e41f72d5e9df1e4_large.jpeg'     
-
-    },
-    {
-      index:1,
-      titre: 'janne' , 
-      status : 'attenteAjout',
-      name:'MyName',
-      img:'http://img0.mxstatic.com/wallpapers/44e535006cffbc1b6e41f72d5e9df1e4_large.jpeg'
-    },
-    {
-      index:2,
-      titre: 'romain' , 
-      status : 'recomandéPar',
-      recomandation : 'hugo',
-      name:'MyName',
-      img:'http://img0.mxstatic.com/wallpapers/44e535006cffbc1b6e41f72d5e9df1e4_large.jpeg'
-    },
-    {
-      index:4,
-      titre: 'romain', 
-      status : 'invitationEncours',
-      name:'MyName',
-      img:'http://img0.mxstatic.com/wallpapers/44e535006cffbc1b6e41f72d5e9df1e4_large.jpeg'
-    },
-    {
-      index:5,
-      titre: 'catherine', 
-      status : 'recomandéPar',
-      recomandation : 'lucien',
-      name:'MyName',
-      img:'http://img0.mxstatic.com/wallpapers/44e535006cffbc1b6e41f72d5e9df1e4_large.jpeg'
-    }
-    ];
-
-    // Promise-based API
+  .factory('Amis', function($http) {
     return {
-      loadAllFaq  : function () {
-      return AMIS
-      }
-    };
-  
-
-})
-})();
+        listeAmisRequest: function(data) {
+            return $http.get('/listeAmis')
+            .success(function (data) {
+              console.log(data)
+            })
+            .error(function (data) {
+              console.log(data)
+            })
+        };
+    }
+  });
