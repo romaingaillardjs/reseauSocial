71beb1238304c349c42a80530592e463f66f86ca
romaingaillard <romain.gaillard.ifocop@gmail.com>
2016-07-01 15:01:27 +0200

    01/07/2016

--- public/js/controllers/listeAmis.js
+++ public/js/controllers/listeAmis.js
@@ -2,7 +2,7 @@ angular.module('MyApp')
   .controller('ListeAmisCtrl', function($scope, Amis) {
     $scope.listeAmis = function (data) {
       Amis.listeAmisRequest().success(function (data) {
-            $scope.amis = data
+            $scope.amis = data;
           });    
           return $scope.amis;
       }
