5e973e29919db366f7cff14e1e35d050eceff5fc
romaingaillardjs <romain.gaillard.ifocop@gmail.com>
2016-07-02 14:15:59 +0200

    02/07/2016

--- public/js/services/amis.js
+++ public/js/services/amis.js
@@ -2,7 +2,7 @@ angular.module('MyApp')
   .factory('Amis', function($http, $window) {
     return {
         listeAmisRequest: function() {
-            return angular.fromJson($window.localStorage.user.ami) 
+            return angular.fromJson($window.localStorage.user) 
      	}
     }
   });
