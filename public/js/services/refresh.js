a9f12b234c8d137758a415142bab5e8a40774c05
romaingaillardjs <romain.gaillard.ifocop@gmail.com>
2016-07-10 15:53:47 +0200

    09/07/2016

--- public/js/services/refresh.js
+++ public/js/services/refresh.js
@@ -6,7 +6,7 @@ angular.module('MyApp')
           return $http.post('/profil', {id:data})
           .success(function(response) {
             console.log(response)
-          $window.localStorage.user = response.data;
+          $window.localStorage.user = response.data.user;
           console.log($window.localStorage.user)
         }).error(function  (data) {
           console.log(data)
