e7aa4b1e95cdadd9f5e581b8f26fd4e4c2a9242e
romaingaillardjs <romain.gaillard.ifocop@gmail.com>
2016-07-14 16:06:27 +0200

    14/07/2016

--- public/js/controllers/messages.js
+++ public/js/controllers/messages.js
@@ -2,39 +2,6 @@ angular.module('MyApp')
   .controller('MessagesCtrl', function($scope, $rootScope, $window, $timeout,$interval ,$location,$anchorScroll, Contact, Message) {
 $scope.user = $rootScope.currentUser;
 
-
-$scope.gotoBottom = function() {
-      $location.hash('bottom');
-      $anchorScroll();
-    };
-    $scope.gotoBottom()
-    $scope.envoyerMessagePrives = function (user_id, message) {
-      Message.postMessagePrives(user_id, message)
-        .then(function(response) {
-          $scope.messages = {
-            success: [response.data]
-          };
-          $scope.texteduMessage ='';
-        })
-        .catch(function(response) {
-          $scope.messages = {
-            error: Array.isArray(response.data) ? response.data : [response.data]
-          };
-        });
-    };
-
-    $scope.listeAmis = function (data) {
-      return Message.listeAmis(data)
-        .success(function (data) {
-        $scope.amis = data 
-      })
-    };
-    $scope.listeAmis($scope.user.ami).success(function (data) {
-    $scope.checkNbMessage()
-    })
-
-
-
 $scope.nbNoViewMessage = function (id) {    
       return Message.countNoViewMessage(id)
           .success(function (data) {
@@ -56,37 +23,79 @@ $scope.checkNbMessage = function () {
       $scope.getnbNoViewMessage($scope.amis[""+i+""]._id,i)
     }
 }
-    $scope.voirAmisMessages = function (id,name) {
-      console.log(id)
-    Message.getMessagesPrives(id)
-            .success(function (data) {
-              $scope.MessagesPrivesRecus = data; 
-              $timeout(function () {
-                $scope.gotoBottom()
-              }, 500 )
-              $scope.gotoBottom()
-            })
-      
-      if (angular.isDefined(stop)) {$interval.cancel(stop)};
-        stop = $interval(function() {
-          Message.getMessagesPrives(id)
-            .success(function (data) {
-              console.log(data)
-              
-              if (($scope.MessagesPrivesRecus.length > 0) && (data.length > $scope.MessagesPrivesRecus.length)) {
+$scope.majNbMessage = function() {
 
-                   $scope.MessagesPrivesRecus = data; 
-                   $timeout(function () {
-                     $scope.gotoBottom()
-                   },500)
-              }
-            })
-          }, 1000);
+  if (MajMessage!==undefined) 
+    {
+      $interval.cancel(MajMessage)
+    }
+    MajMessage = $interval(function() {
+      $scope.listeAmis($scope.user.ami)
+        .success(function (data) {
+          $scope.checkNbMessage()
+        })
+    },3000)
+}
+$scope.gotoBottom = function() {
+      $location.hash('bottom');
+      $anchorScroll();
+    };
+    $scope.gotoBottom()
 
-      $scope.checkNbMessage()
-      $scope.select = id
-      $scope.currentAmiMessageName = name;
-
+$scope.envoyerMessagePrives = function (user_id, message) {
+  Message.postMessagePrives(user_id, message)
+    .then(function(response) {
+      $scope.messages = {
+        success: [response.data]
       };
-    
+      $scope.texteduMessage ='';
+    })
+    .catch(function(response) {
+      $scope.messages = {
+        error: Array.isArray(response.data) ? response.data : [response.data]
+      };
+    });
+};
+
+$scope.listeAmis = function (data) {
+  return Message.listeAmis(data)
+    .success(function (data) {
+    $scope.amis = data 
   })
+};
+$scope.listeAmis($scope.user.ami).success(function (data) {
+$scope.checkNbMessage()
+$scope.majNbMessage()
+})
+
+$scope.voirAmisMessages = function (id,name) {
+  Message.getMessagesPrives(id)
+    .success(function (data) {
+      $scope.MessagesPrivesRecus = data; 
+      $timeout(function () {
+        $scope.gotoBottom()
+      }, 500 )
+      $scope.gotoBottom()
+    })
+    if (angular.isDefined(stop)) 
+      {
+        $interval.cancel(stop)
+      }
+    stop = $interval(function() {
+      Message.getMessagesPrives(id)
+        .success(function (data) {
+        console.log(data)
+        if (($scope.MessagesPrivesRecus.length > 0) && (data.length > $scope.MessagesPrivesRecus.length)) 
+          {
+            $scope.MessagesPrivesRecus = data; 
+            $timeout(function () {
+             $scope.gotoBottom()
+            },500)
+          }
+      })
+    }, 2000);
+
+  $scope.select = id
+  $scope.currentAmiMessageName = name;
+  }; 
+})
